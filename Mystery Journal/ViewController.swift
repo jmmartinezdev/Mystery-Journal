ff40dff670c4778310e02d6b9a8c6ef9d3b844d0
Chema Martinez <jmmartinezdev@gmail.com>
2021-12-29 12:58:29 +0100

    Initial Commit

---
+++ Mystery Journal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mystery Journal
+//
+//  Created by Chema Martinez on 29/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
